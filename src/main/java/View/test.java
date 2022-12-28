22d68d992102e15e3d0334e2b517b75424f910c5
emadnawarr <emadnawar2009@gmail.com>
2022-12-28 19:04:46 +0200

    main menu

--- src/main/java/View/test.java
+++ src/main/java/View/test.java
@@ -6,13 +6,49 @@ package View;
 
 import eg.edu.alexu.csd.oop.game.*;
 import eg.edu.alexu.csd.oop.game.GameEngine.GameController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
 /**
  *
  * @author PC
  */
 public class test {
-    public static void main(String[] args) {
-            final GameController gameController = GameEngine.start("zizo", new CircusOfPlates(800, 600));
 
+    public static void main(String[] args) {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New");
+        JMenuItem pauseMenuItem = new JMenuItem("Pause");
+        JMenuItem resumeMenuItem = new JMenuItem("Resume");
+        menu.add(newMenuItem);
+        menu.addSeparator();
+        menu.add(pauseMenuItem);
+        menu.add(resumeMenuItem);
+        menuBar.add(menu);
+        final GameController gameController = GameEngine.start("Zizo", new CircusOfPlates(800, 600), menuBar, Color.BLACK);
+        newMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // gameController.changeWorld(new CicusOfPlates(800));
+            }
+        });
+        pauseMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameController.pause();
+            }
+        });
+        resumeMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameController.resume();
+            }
+        });
     }
+
 }
